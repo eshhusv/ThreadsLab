6df50306ac1e95e34b2921281dabaefe24bd7ac6
ak752 <ak7527203@gmail.com>
2023-09-22 01:53:52 +0200

    g

--- ThreadsLab/Program.cs
+++ ThreadsLab/Program.cs
@@ -2,17 +2,10 @@
 
 internal class Program
 {
-    static void SecondThread()
+    static void SecondThread(int num1, int num2, int num3)
     {
-        Console.Write("Введите первое число: ");
-        int num1 = int.Parse(Console.ReadLine());
-        Console.Write("Введите второе число: ");
-        int num2 = int.Parse(Console.ReadLine());
-        Console.Write("Введите третье число: ");
-        int num3 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine(Thread.CurrentThread.Name + " начал работу");
-
+        Thread.CurrentThread.Name = "SecondThread";
+        Console.WriteLine(Thread.CurrentThread.Name + " has started working");
         Stopwatch sw = Stopwatch.StartNew();
         sw.Start();
         int[] nums = { num1, num2, num3 };
@@ -33,17 +26,47 @@ internal class Program
         }
         sw.Stop();
 
-        Console.WriteLine(Thread.CurrentThread.Name + " закончил работу");
 
-        Console.WriteLine($"НОД чисел {num1}, {num2} и {num3}: " + nod);
-        Console.WriteLine("Stopwatch: " + sw.ElapsedTicks.ToString());
-
+        Console.WriteLine(Thread.CurrentThread.Name + $": GCD {num1}, {num2} и {num3}: " + nod);
+        Console.WriteLine(Thread.CurrentThread.Name + "Stopwatch: " + sw.ElapsedTicks.ToString());
+        Console.WriteLine(Thread.CurrentThread.Name + " has ended working");
     }
 
     private static void Main(string[] args)
     {
-        ThreadStart secondThread = new ThreadStart(SecondThread);
+        Console.Write("Type first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Type second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.Write("Type third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+        ThreadStart secondThread = new ThreadStart(SecondThread(num1, num2, num3));
         Thread thread = new Thread(secondThread);
+        Thread.CurrentThread.Name = "MainThread";
         thread.Start();
+
+        Stopwatch sw = Stopwatch.StartNew();
+        sw.Start();
+        int[] nums = { num1, num2, num3 };
+        int min = num1;
+        foreach (int num in nums)
+        {
+            if (min > num) min = num;
+        }
+
+        int nod;
+
+        if (num1 > 0 & num2 > 0 & num3 > 0) nod = 1;
+        else nod = 0;
+
+        for (int i = 1; i < min + 1; i++)
+        {
+            if (num1 % i == 0 & num2 % i == 0 & num3 % i == 0) nod = i;
+        }
+        sw.Stop();
+
+        Console.WriteLine(Thread.CurrentThread.Name + $"GCD {num1}, {num2} и {num3}: " + nod);
+        Console.WriteLine(Thread.CurrentThread.Name + "Stopwatch: " + sw.ElapsedTicks.ToString());
+        Console.WriteLine(Thread.CurrentThread.Name + " has ended working");
     }
 }
